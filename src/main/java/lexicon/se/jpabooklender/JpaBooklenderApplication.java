14e12b4525017ed31b79f21939a9bb3387083716
Bigom <bigom2199@gmail.com>
2023-10-18 12:25:51 +0200

    Initial commit

---
+++ src/main/java/lexicon/se/jpabooklender/JpaBooklenderApplication.java
@@ -0,0 +1,13 @@
+package lexicon.se.jpabooklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBooklenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBooklenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lexicon/se/jpabooklender/JpaBooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package lexicon.se.jpabooklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBooklenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
